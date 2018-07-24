418351d5a440a9822477117dd8f9ff95f0bfa2e9
Hemanth <tammareddy53@gmail.com>
2018-07-24 13:23:10 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -156,6 +156,9 @@ def echo_post(request):
     # Parse POST JSON body
     print("Request Info oneeeeee %s",request)
     print("Request Info twoooooooo %s",request.content.read())
+    print("JSONNNN  %s",json.loads(request.content.read())) 
+    what = json.loads(request.content.read())
+    print("onetimepasswordonetimepassword  %s",what.onetimepassword) 
     body = json.loads(request.content.read().decode("utf-8"))
     onetimepassword  = body.onetimepassword
     wifkey = body.wifkey

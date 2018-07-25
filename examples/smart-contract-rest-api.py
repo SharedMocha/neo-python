5710091af70c95770da7df9b37d318ddd3290cc8
Hemanth <tammareddy53@gmail.com>
2018-07-25 12:20:08 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Test
+
 """
 Example of running a NEO node, receiving smart contract notifications and
 integrating a simple REST API.
@@ -23,7 +27,7 @@ The REST API is using the Python package 'klein', which makes it possible to
 create HTTP routes and handlers with Twisted in a similar style to Flask:
 https://github.com/twisted/klein
 """
-#Test
+
 import os
 import threading
 import datetime
@@ -32,8 +36,6 @@ import time
 from time import sleep
 import re
 import requests
-
-
 
 from logzero import logger
 from twisted.internet import reactor, task, endpoints
@@ -45,9 +47,11 @@ from neo.Core.Blockchain import Blockchain
 from neo.Implementations.Blockchains.LevelDB.LevelDBBlockchain import LevelDBBlockchain
 from neo.Settings import settings
 
-from neo.Network.api.decorators import json_response, gen_authenticated_decorator, catch_exceptions
+from neo.Network.api.decorators import json_response, \
+    gen_authenticated_decorator, catch_exceptions
 from neo.contrib.smartcontract import SmartContract
-from neo.SmartContract.ContractParameter import ContractParameter, ContractParameterType
+from neo.SmartContract.ContractParameter import ContractParameter, \
+    ContractParameterType
 
 from neo.Wallets.utils import to_aes_key
 from prompt_toolkit.styles import Style
@@ -55,76 +59,94 @@ from neo.UserPreferences import preferences
 from neo.Implementations.Wallets.peewee.UserWallet import UserWallet
 from twisted.internet import reactor, task
 
-
-
 # Set the hash of your contract here:
-SMART_CONTRACT_HASH = "6537b4bd100e514119e3a7ab49d520d20ef2c2a4"
+
+SMART_CONTRACT_HASH = '6537b4bd100e514119e3a7ab49d520d20ef2c2a4'
 
 # Default REST API port is 8080, and can be overwritten with an env var:
-API_PORT = os.getenv("NEO_REST_API_PORT", 8080)
+
+API_PORT = os.getenv('NEO_REST_API_PORT', 8080)
 
 # If you want to enable logging to a file, set the filename here:
-LOGFILE = os.getenv("NEO_REST_LOGFILE", None)
+
+LOGFILE = os.getenv('NEO_REST_LOGFILE', None)
 
 # Internal: if LOGFILE is set, file logging will be setup with max
 # 10 MB per file and 3 rotations:
+
 if LOGFILE:
     settings.set_logfile(LOGFILE, max_bytes=1e7, backup_count=3)
 
 # Internal: get the API token from an environment variable
-API_AUTH_TOKEN = os.getenv("NEO_REST_API_TOKEN", None)
+
+API_AUTH_TOKEN = os.getenv('NEO_REST_API_TOKEN', None)
 if not API_AUTH_TOKEN:
-    raise Exception("No NEO_REST_API_TOKEN environment variable found!")
+    raise Exception('No NEO_REST_API_TOKEN environment variable found!')
 
 # Internal: setup the smart contract instance
+
 smart_contract = SmartContract(SMART_CONTRACT_HASH)
 
 # Internal: setup the klein instance
+
 app = Klein()
 
 # Internal: generate the @authenticated decorator with valid tokens
+
 authenticated = gen_authenticated_decorator(API_AUTH_TOKEN)
+
 
 #
 # Smart contract event handler for Runtime.Notify events
 #
 
-
 @smart_contract.on_notify
 def sc_notify(event):
-    logger.info("SmartContract Runtime.Notify event: %s", event)
+    logger.info('SmartContract Runtime.Notify event: %s', event)
 
     # Make sure that the event payload list has at least one element.
-    if not isinstance(event.event_payload, ContractParameter) or event.event_payload.Type != ContractParameterType.Array or not len(event.event_payload.Value):
+
+    if not isinstance(event.event_payload, ContractParameter) \
+        or event.event_payload.Type != ContractParameterType.Array \
+        or not len(event.event_payload.Value):
         return
 
     # The event payload list has at least one element. As developer of the smart contract
     # you should know what data-type is in the bytes, and how to decode it. In this example,
     # it's just a string, so we decode it with utf-8:
-    logger.info("- payload part 1: %s", event.event_payload.Value[0].Value.decode("utf-8"))
+
+    logger.info('- payload part 1: %s',
+                event.event_payload.Value[0].Value.decode('utf-8'))
 
 
 #
 # Custom code that runs in the background
 #
+
 class PromptInterface:
+
     token_style = None
     start_height = None
     start_dt = None
     Wallet = None
+
     def __init__(self):
-        #self.input_parser = InputParser()
+
+        # self.input_parser = InputParser()
+
         self.start_height = Blockchain.Default().Height
         self.start_dt = datetime.datetime.utcnow()
-        logger.info("TESTTT Block %s / %s", str(self.start_height), str(self.start_dt))
+        logger.info('TESTTT Block %s / %s', str(self.start_height),
+                    str(self.start_dt))
 
         self.token_style = Style.from_dict({
-            "command": preferences.token_style['Command'],
-            "neo": preferences.token_style['Neo'],
-            "default": preferences.token_style['Default'],
-            "number": preferences.token_style['Number'],
-        })
-        
+            'command': preferences.token_style['Command'],
+            'neo': preferences.token_style['Neo'],
+            'default': preferences.token_style['Default'],
+            'number': preferences.token_style['Number'],
+            })
+
+
 def custom_background_code():
     """ Custom code run in a background thread. Prints the current block height.
 
@@ -132,17 +154,20 @@ def custom_background_code():
     moment, whenever the main thread quits. If you need more safety, don't use a  daemonized
     thread and handle exiting this thread in another way (eg. with signals and events).
     """
+
     while True:
-        logger.info("Block %s / %s", str(Blockchain.Default().Height), str(Blockchain.Default().HeaderHeight))
+        logger.info('Block %s / %s', str(Blockchain.Default().Height),
+                    str(Blockchain.Default().HeaderHeight))
         sleep(15)
 
 
 #
 # REST API Routes
 #
+
 @app.route('/')
 def home(request):
-    return "Hello world"
+    return 'Hello world'
 
 
 @app.route('/echo/<msg>')
@@ -150,13 +175,13 @@ def home(request):
 @authenticated
 @json_response
 def echo_msg(request, msg):
-    #promptInterface = PromptInterface();
+
+    # promptInterface = PromptInterface();
+
     height = Blockchain.Default().Height
     headers = Blockchain.Default().HeaderHeight
-    msg = headers-height
-    return {
-        "echo": msg
-    }
+    msg = headers - height
+    return {'echo': msg}
 
 
 @app.route('/echo-post', methods=['POST'])
@@ -164,10 +189,12 @@ def echo_msg(request, msg):
 @authenticated
 @json_response
 def echo_post(request):
+
     # Parse POST JSON body
-    body = json.loads(request.content.read().decode("utf-8"))
-    print("Incomming Body %s" % body)
-    onetimepassword  = body['onetimepassword']
+
+    body = json.loads(request.content.read().decode('utf-8'))
+    print 'Incomming Body %s' % body
+    onetimepassword = body['onetimepassword']
     wifkey = body['wifkey']
     sc_location = body['smart_contract_location']
     r = requests.get(sc_location, allow_redirects=True)
@@ -176,47 +203,61 @@ def echo_post(request):
     localtime = str(time.time())  # this removes the decimals
     temp_filename = localtime + str(password_key)
     filename = re.sub('[^ a-zA-Z0-9]', '', temp_filename)
-    path = "/home/ubuntu/"+filename
-    scname = path+".py"
-    print("Incomming FilePath %s" % scname)
-    returnvalue = "Issue in creating wallet.Please try manual approach"
-    #Save SC.py file
+    path = '/home/ubuntu/' + filename
+    scname = path + '.py'
+    print 'Incomming FilePath %s' % scname
+    returnvalue = 'Issue in creating wallet.Please try manual approach'
+
+    # Save SC.py file
+
     try:
         open(scname, 'wb').write(r.content)
-    except Exception as e:
-        print("Exception creating file: %s" % e)
-        return "Issue Downloading and Saving your smart contract.Please try manual approach"
-    #Create Wallet	
+    except Exception, e:
+        print 'Exception creating file: %s' % e
+        return 'Issue Downloading and Saving your smart contract.Please try manual approach'
+
+    # Create Wallet....
+
     try:
-        walletinfo.Wallet = UserWallet.Create(path=path,password=password_key)
-	#contract = walletinfo.Wallet.GetDefaultContract()
-	#key = walletinfo.Wallet.GetKey(contract.PublicKeyHash)
-	returnvalue = walletinfo.Wallet.ToJson()
-	print("Wallet %s" % json.dumps(walletinfo.Wallet.ToJson(), indent=4))
-	walletinfo._walletdb_loop = task.LoopingCall(walletinfo.Wallet.ProcessBlocks)
-	walletinfo._walletdb_loop.start(1)
-	print("Wallet Oppend")
-	#print("Pubkey %s" % key.PublicKey.encode_point(True))
-    except Exception as e:
-        print("Exception creating wallet: %s" % e)
+        walletinfo.Wallet = UserWallet.Create(path=path,
+                password=password_key)
+
+    # contract = walletinfo.Wallet.GetDefaultContract()
+    # key = walletinfo.Wallet.GetKey(contract.PublicKeyHash)
+
+        returnvalue = walletinfo.Wallet.ToJson()
+        print 'Wallet %s' % json.dumps(walletinfo.Wallet.ToJson(),
+                indent=4)
+        walletinfo._walletdb_loop = \
+            task.LoopingCall(walletinfo.Wallet.ProcessBlocks)
+        walletinfo._walletdb_loop.start(1)
+        print 'Wallet Oppend'
+    except Exception, e:
+
+    # print("Pubkey %s" % key.PublicKey.encode_point(True))
+
+        print 'Exception creating wallet: %s' % e
         walletinfo.Wallet = None
         return returnvalue
-    #Open and Replace Wallet
+
+    # Open and Replace Wallet
     # Echo it
-    return {
-        "post-body": returnvalue
-    }
+
+    return {'post-body': returnvalue}
+
 
 #
 # Main method which starts everything up
 #
 
-
 def main():
+
     # Use TestNet
+
     settings.setup_testnet()
 
     # Setup the blockchain
+
     blockchain = LevelDBBlockchain(settings.chain_leveldb_path)
     Blockchain.RegisterBlockchain(blockchain)
     dbloop = task.LoopingCall(Blockchain.Default().PersistBlocks)
@@ -224,16 +265,19 @@ def main():
     NodeLeader.Instance().Start()
 
     # Disable smart contract events for external smart contracts
+
     settings.set_log_smart_contract_events(False)
 
     # Start a thread with custom code
+
     d = threading.Thread(target=custom_background_code)
     d.setDaemon(True)  # daemonizing the thread will kill it when the main thread is quit
     d.start()
 
     # Hook up Klein API to Twisted reactor.
     # endpoint_description = "tcp:port=%s:interface=localhost" % API_PORT
-    endpoint_description = "tcp:port=%s" % API_PORT
+
+    endpoint_description = 'tcp:port=%s' % API_PORT
 
     # If you want to make this service externally available (not only at localhost),
     # then remove the `interface=localhost` part:
@@ -243,10 +287,11 @@ def main():
     endpoint.listen(Site(app.resource()))
 
     # Run all the things (blocking call)
-    logger.info("Everything setup and running. Waiting for events...")
+
+    logger.info('Everything setup and running. Waiting for events...')
     reactor.run()
-    logger.info("Shutting down.")
+    logger.info('Shutting down.')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

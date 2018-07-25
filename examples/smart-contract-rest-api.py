efe333c3ffac517564c833743f3f5db77a23d8af
Hemanth <tammareddy53@gmail.com>
2018-07-25 15:41:31 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -225,7 +225,7 @@ def echo_post(request):
     try:
         print ('4 ----4 -> Starting core process')
         Blockchain.Default().Pause()
-        BuildAndRun(scname, walletinfo.Wallet)
+        BuildAndRun(scname, this.walletinfo.Wallet)
         Blockchain.Default().Resume()
         print ('5 ----5 -> .avm file created')
         args = []

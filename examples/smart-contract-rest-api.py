a8cbf70a38c0ab71caf0a399c99fba0f53e2d50a
Hemanth <tammareddy53@gmail.com>
2018-07-26 10:17:49 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -256,8 +256,9 @@ def echo_post(request):
         function_code_json = json.dumps(function_code.ToJson())
         print ("------------------------------------- %s", function_code_json)
         sc_hash = function_code_json['hash']
+        print("================== %s",sc_hash)
         hash_json_success = json.dumps({'status':'success', 'hash':sc_hash,'details': 'Wait for few minutes before you try invoke on your smart contract.'})
-        print("================== %s",hash_json_success)
+        print("=================________= %s",hash_json_success)
         if function_code:
             contract_script = GatherContractDetails(function_code)
             print ('7 ----7 -> contract_script completed')          

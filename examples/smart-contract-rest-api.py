c8fc57fde948f740e4a8c9894e5a30cc7a6c0710
Hemanth <tammareddy53@gmail.com>
2018-07-26 10:02:39 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -250,7 +250,7 @@ def echo_post(request):
         args, from_addr = get_from_addr(args)
         function_code = LoadContract(args[1:])
         hash_json_failed = json.dumps({'status':'failed', 'reason': 'Contract Not Deployed due to issues (or) Insufficient Balance.Please try manual approach.'})
-        function_code_json = json.dumps(function_code.ToJson()
+        function_code_json = json.dumps(function_code.ToJson())
         sc_hash = function_code_json.hash
         hash_json_success = json.dumps({'status':'success', 'hash':sc_hash,'details': 'Wait for few minutes before you try invoke on your smart contract.'})
         if function_code:

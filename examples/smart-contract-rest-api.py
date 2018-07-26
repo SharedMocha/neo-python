d2e628f07a0ca697ef48315f72ea4acbdc7bfdfc
Hemanth <tammareddy53@gmail.com>
2018-07-26 10:00:35 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -249,6 +249,10 @@ def echo_post(request):
         print ('6 ----5.4')
         args, from_addr = get_from_addr(args)
         function_code = LoadContract(args[1:])
+        hash_json_failed = json.dumps({'status':'failed', 'reason': 'Contract Not Deployed due to issues (or) Insufficient Balance.Please try manual approach.'})
+        function_code_json = json.dumps(function_code.ToJson()
+        sc_hash = function_code_json.hash
+        hash_json_success = json.dumps({'status':'success', 'hash':sc_hash,'details': 'Wait for few minutes before you try invoke on your smart contract.'})
         if function_code:
             contract_script = GatherContractDetails(function_code)
             print ('7 ----7 -> contract_script completed')          
@@ -267,18 +271,23 @@ def echo_post(request):
                     print(
                         "-------------------------------------------------------------------------------------------------------------------------------------\n")
                     result = InvokeContract(walletinfo.Wallet, tx, Fixed8.Zero(), from_addr=from_addr)
-                    return result
+                    if result:
+                        return hash_json_failed
+                    else:
+                        return hash_json_failed
+                                        
+                    #return result
                 else:
                     print("Test invoke failed")
                     print("TX is %s, results are %s" % (tx, results))
-                    return "Test invoke failed"                   
+                    return hash_json_failed                   
     except Exception as e:
 
     # print("Pubkey %s" % key.PublicKey.encode_point(True))
 
         print ('Exception creating wallet: %s' % e)
         walletinfo.Wallet = None
-        return returnvalue
+        return hash_json_failed
 
     # Open and Replace Wallet
     # Echo it

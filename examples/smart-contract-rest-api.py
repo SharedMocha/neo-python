919d13287bc551ca54a7d736e173f3bcd3073d1a
Hemanth <tammareddy53@gmail.com>
2018-07-27 14:03:32 -0700

    Update smart-contract-rest-api.py

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -213,7 +213,7 @@ def echo_post(request):
     avmname = '/'+path+'.avm'
     print ('3 ----3 -> Incomming FilePath %s' % scname)
     returnvalue = 'Issue in creating wallet.Please try manual approach'
-    failed_data = {"status": "failed", "reason": "Contract Not Deployed due to issues such as **smart_contract_location: not ending with .py and the link might not be raw url. Click raw button on github filename to get the correct url** in your request body (or) Insufficient Balance in the wallet.Please try manual approach."}
+    failed_data = {"status": "failed", "reason": "Contract Not Deployed due to issues such as **smart_contract_location: not ending with .py and/or smart_contract_location link might not be raw url.Click raw button on your github file to get the correct url**. Issue might also be caused by insufficient balance in the wallet.Please try manual approach or caht with @sharedmocha#8871 on discord."}
     hash_json_failed = failed_data
 
     # Save SC.py file
